224b2137f62aa55a394b023327e26058c4db24d1
RuthRivera2004 <riveraruth861@gmail.com>
2026-09-18 17:22:31 -0600

    -implementacion de pantallas y tab navigator.

--- Lab/src/components/navigation/TabsNavigator.tsx
+++ Lab/src/components/navigation/TabsNavigator.tsx
@@ -1,9 +1,37 @@
-import { View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useTheme } from "../context/ThemeContext";
+import HomeScreen from "../screen/HomeScreen";
+import { Ionicons } from "@expo/vector-icons";
+import ExploreScreen from "../screen/ExploreScreen";
+import SettingsScreen from "../screen/SettingsScreen";
 
-export default function TabsNavigator(){
-    return(
-        <View>
-            
-        </View>
-    )
+
+const Tab = createBottomTabNavigator();
+
+export default function TabsNavigator() {
+  const { colors, isDark} = useTheme();
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor:   colors.primary,
+        tabBarInactiveTintColor: colors.textSecondary,
+        tabBarStyle:  { backgroundColor: colors.surface },
+        headerStyle:  { backgroundColor: colors.surface },
+        headerTintColor: colors.text,
+      }}
+    >
+      <Tab.Screen name='Inicio' component={HomeScreen}
+        options={{ tabBarIcon: ({ color, size }) =>
+          <Ionicons name='home' size={size} color={color} /> }} />
+
+      <Tab.Screen name='Explorar' component={ExploreScreen}
+        options={{ tabBarIcon: ({ color, size }) =>
+          <Ionicons name='grid' size={size} color={color} /> }} />
+
+      <Tab.Screen name='Configuracion' component={SettingsScreen}
+        options={{ tabBarIcon: ({ color, size }) =>
+          <Ionicons name='settings' size={size} color={color} /> }} />
+    </Tab.Navigator>
+  );
 }
